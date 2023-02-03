4fd314e6672e4d3dae6725d4af9080fdea0ad700
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2023-02-03 13:16:10 +0100

    bouton "me contacter"

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,9 +1,22 @@
 <template>
-  <main class="bg-black text-white">
-    <h1 class="text-3xl">Florentin </h1>
-    <h1 class="text-3xl">Demortiere</h1>
+  <main class="bg-black text-white font-raleway font-semibold">
+    <div>
+      <div class="p-8 mt-20">
+        <h1 class="text-3xl">Florentin </h1>
+        <h1 class="text-3xl ml-3 m-1">Demortiere</h1>
+      </div>
+      <div>
+        <p class="text-xs m-12 pr-8 -mt-6 opacity-50">Je m'appelle Florentin Demortiere et je suis un étudiant en MMI. Je suis passionné par le design graphique</p>
+      </div>
+    </div>
+    <div class="">
+      <button class="bg-white flex gap-2 px-6 py-2 rounded-full place-items-center ml-10">
+        <p class="text-black font-bold">Me contacter</p>
+        <img class="" src="images/fleche.png" alt="fleche">
+      </button>
+    </div>
+      <img class="w-80 absolute bottom-12 -right-20" src="gif_portefolio.gif" alt="gif animé">
 
-    <img class="w-80 absolute bottom-0 -right-10" src="gif_portefolio.gif" alt="gif animé">
   </main>
 </template>
 
