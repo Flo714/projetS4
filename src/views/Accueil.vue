ecd975b8ce4b17e497f16ea3b4be1910443a0cac
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2023-02-24 08:54:22 +0100

    blur + logo fond début

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,8 +1,10 @@
 <template>
   <main class="bg-black text-white font-raleway font-semibold">
-    <div class="bg-[url('/images/Logo_portefolio.png')] static">
-    </div>
     <div>
+      <img class="w-80 absolute top-0 right-0" src="/images/Ellipse.png" alt="">
+    </div>
+    <img class="fixed ml-52opacity-40" src="/images/Logo_portefolio.png" alt="Logo">
+    <div class="">
         <div class="p-8 mt-20">
           <h1 class="text-3xl">Florentin </h1>
           <h1 class="text-3xl ml-3 m-1">Demortiere</h1>
@@ -12,12 +14,17 @@
         </div>
       </div>
       <div class="">
-        <button class="bg-white flex gap-2 px-6 py-2 rounded-full place-items-center ml-10">
-          <p class="text-black font-bold">Me contacter</p>
+        <button class="bg-white flex gap-2 px-3 py-2 rounded-full place-items-center ml-10">
+          <p class="text-black font-bold text-sm">Me contacter</p>
           <img class="" src="images/fleche.png" alt="fleche">
         </button>
       </div>
+      <div>
         <img class="w-40 absolute bottom-12 right-4" src="gif1_pf.gif" alt="gif animé">
+      </div>
+      <div class="my-40">
+        <h2>Mes Projets</h2>
+      </div>
   </main>
 </template>
 
