906c6cc23e435f379fbb4fe9c2c955d41f5461f8
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2023-03-09 19:29:34 +0100

    responsive projet 1

--- src/views/Logo_Florentin_Demortiere.vue
+++ src/views/Logo_Florentin_Demortiere.vue
@@ -61,18 +61,18 @@
   </nav>
   </header>
   <main class="text-white font-raleway">
-    <h1 class="text-lg font-bold my-10 mx-6">Réalisation de mon logo personnel <br>et du motion design</h1>
+    <h1 class="text-lg font-bold my-10 mx-6">Réalisation de mon logo personnel et du motion design</h1>
+    <p class="opacity-80 text-sm px-4 lg:mr-[500px] lg:mx-20">C'est un logo personnel qui représente mes passions et mes aspirations. J'ai choisi de travailler avec Illustrator pour créer un logo qui associe les lettres F et D de manière créative et originale.</p>
+    <div class=" my-10 md:flex md:mx-16 lg:justify-start lg:gap-10 lg:mx-2">
+      <img class="w-48 mx-auto lg:ml-20 lg:mr-0  my-6 md:w-64 lg:w-80" src="images/Logo_portefolio.png" alt="Logo personnel">
 
-    <div class=" my-10 md:flex md:mx-16">
-      <img class="w-48 mx-auto my-6" src="images/Logo_portefolio.png" alt="">
-
-      <p class="text-center my-12">Création d'un logo personnel avec Illustrator</p>
+      <p class="text-center my-12 lg:text-xl italic lg:mt-20 lg:ml-10">Création d'un logo personnel avec Illustrator</p>
     </div>
-    <div class="my-12 md:flex md:mx-16 md:content-center">
-      <video width="200" height="" controls class="mx-auto mt-10">
+    <div class="my-12 md:flex md:mx-16 md:items-center lg:justify-start lg:gap-10 lg:mx-2">
+      <video width="200" height="" controls class="mx-auto mt-10 md:w-64 lg:w-80 lg:ml-20 lg:mr-0">
         <source src="video/Logo_Animation.mp4">
       </video>
-      <p class="text-center my-8">Vidéo réalisée à l'aide d'After Effect</p>
+      <p class="text-center my-8 lg:text-xl italic lg:w-80 lg:ml-10">Vidéo réalisée à l'aide d'After Effect</p>
     </div>
     <div class="text-xs italic flex justify-between mx-2 my-1 mt-20">
       <p class="">Florentin Demortiere</p>
