b856544f92f02f1ae5f8aa3bbef00cf1a7926d8d
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-18 21:49:21 +0100

    Update Charte_graphique_UFC.vue

--- src/views/Charte_graphique_UFC.vue
+++ src/views/Charte_graphique_UFC.vue
@@ -91,13 +91,13 @@
     <h2 class="font-bold text-lg lg:text-4xl mt-10 mb-20 mx-10 ">Galerie Photo</h2>
     <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-items-center items-center gap-10 lg:gap-36 xl:gap-64 ">
       <div class="lg:mx-20 z-20">
-        <img class="max-w-[200px]  lg:max-w-md xl:max-w-lg" src="/public/images/Couleurs.jpg" alt="Image des couleurs de la charte graphique de l'université de Franche-Comté">
+        <img class="max-w-[200px]  lg:max-w-md" src="/public/images/Couleurs.jpg" alt="Image des couleurs de la charte graphique de l'université de Franche-Comté">
       </div>
       <div class="lg:mx-20 z-20">
-        <img class="max-w-[200px] lg:max-w-md xl:max-w-lg" src="/public/images/Applications_varie.jpg" alt="Image de mock-up de la charte graphique de l'université de Franche-Comté">
+        <img class="max-w-[200px] lg:max-w-md" src="/public/images/Applications_varie.jpg" alt="Image de mock-up de la charte graphique de l'université de Franche-Comté">
       </div>
       <div class="lg:mx-20 z-20"> 
-        <img class="max-w-[200px]  lg:max-w-md xl:max-w-lg" src="/public/images/Restriction.jpg" alt="Image des restrictions de la charte graphique de l'université de Franche-Comté">
+        <img class="max-w-[200px]  lg:max-w-md" src="/public/images/Restriction.jpg" alt="Image des restrictions de la charte graphique de l'université de Franche-Comté">
       </div>
     </div>
   </div>
