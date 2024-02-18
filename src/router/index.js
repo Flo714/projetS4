cccdd0cb9f0427aff836aedbd4f763f5ebdb7924
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-18 21:40:50 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/Accueil.vue'
 import Logo_Florentin_Demortiere from '../views/Logo_Florentin_Demortiere.vue'
-import Charte_graphique_Germagny from '../views/Charte_graphique_Germagny.vue'
+import Charte_graphique_UFC from '../views/Charte_graphique_UFC.vue'
 import Motion_design_WebTV from '../views/Motion_design_WebTV.vue'
 import Exposition_Universelle_2025 from '../views/Exposition_Universelle_2025.vue'
 import Aidealareussite from '../views/Aidealareussite.vue'
@@ -13,7 +13,7 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'AccueilView', component: AccueilView },
     { path: '/Logo_Florentin_Demortiere', name: 'Logo_Florentin_Demortiere', component: Logo_Florentin_Demortiere },
-    { path: '/Charte_graphique_Germagny', name: 'Charte_graphique_Germagny', component: Charte_graphique_Germagny },
+    { path: '/Charte_graphique_UFC', name: 'Charte_graphique_UFC', component: Charte_graphique_UFC },
     { path: '/Motion_design_WebTV', name: 'Motion_design_WebTV', component: Motion_design_WebTV },
     { path: '/Exposition_Universelle_2025', name: 'Exposition_Universelle_2025', component: Exposition_Universelle_2025 },
     { path: '/Aidealareussite', name: 'Aidealareussite', component: Aidealareussite },
