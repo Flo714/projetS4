1749ea476b947ca0d0e181fcfb5adba061c14c50
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-19 20:22:34 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import AccueilView from '../views/Accueil.vue'
 import Logo_Florentin_Demortiere from '../views/Logo_Florentin_Demortiere.vue'
 import Charte_graphique_UFC from '../views/Charte_graphique_UFC.vue'
 import Motion_design_WebTV from '../views/Motion_design_WebTV.vue'
-import Drone_video from '../views/Drone_video.vue'
+import ddone_video from '../views/drone_video.vue'
 import Aidealareussite from '../views/Aidealareussite.vue'
 import mentions_legales from '../views/mentions-legales.vue'
 
@@ -15,7 +15,7 @@ const router = createRouter({
     { path: '/Logo_Florentin_Demortiere', name: 'Logo_Florentin_Demortiere', component: Logo_Florentin_Demortiere },
     { path: '/Charte_graphique_UFC', name: 'Charte_graphique_UFC', component: Charte_graphique_UFC },
     { path: '/Motion_design_WebTV', name: 'Motion_design_WebTV', component: Motion_design_WebTV },
-    { path: '/Drone_video', name: 'Drone_video', component: Drone_video },
+    { path: '/drone_video', name: 'drone_video', component: drone_video },
     { path: '/Aidealareussite', name: 'Aidealareussite', component: Aidealareussite },
     { path: '/mentions_legales', name: 'mentions_legales', component: mentions_legales },
  
