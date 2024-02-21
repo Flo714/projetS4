4e9bdb2e39a9ee373bd017f0152dad62520d0365
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-21 21:54:45 +0100

    Update drone_video.vue

--- src/views/drone_video.vue
+++ src/views/drone_video.vue
@@ -90,11 +90,11 @@
 <div class="md:flex md:justify-center md:items-center md:gap-20 xl:gap-56 lg:ml-20 ">
     <div class="z-20">
         <h2 class="font-bold text-lg lg:text-xl">Technique de tournage</h2>
-      <p class="my-4">Pour mon mini-film en drone, j'ai d'abord planifié minutieusement mes prises de vue en repérant les points clés sur la carte. Une fois sur place, j'ai préparé mon équipement, vérifié les conditions météo, et utilisé des techniques variées de vol pour capturer des images dynamiques. En ajustant l'altitude et en filmant aux heures dorées, j'ai réussi à créer un récit visuel immersif mettant en valeur la beauté naturelle de la campagne, tout en respectant les règles de sécurité.</p>
+      <p class="my-4">Toutes les images ont été capturées avec le drone DJI Mini 3 Pro, un équipement de qualité qui m'a permis d'obtenir des prises de vue remarquables pour mon mini-film en campagne. Grâce à ses fonctionnalités avancées et sa portabilité, j'ai pu réaliser des séquences aériennes captivantes tout en conservant une grande facilité d'utilisation sur le terrain.</p>
     </div>
     <div class="my-10 z-20">
-      
-      <p class="text-center my-4 italic">Mini-Film drone</p>
+        <img src="/public/images/Drone_img.png" class="w-[70%] mx-auto lg:max-w-[700px] xl:mx-auto shadow-white shadow" alt="Image de présentation du drone DJI mini 3 Pro">
+      <p class="text-center my-4 italic">DJI Mini 3 Pro</p>
     </div>
   </div>
   <div class="lg:my-40 mx-16 ">
