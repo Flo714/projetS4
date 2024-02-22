beada4c647785d7f6293aa1b0711853ffd2e762b
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-22 22:13:37 +0100

    Update MeetMyGrill.vue

--- src/views/MeetMyGrill.vue
+++ src/views/MeetMyGrill.vue
@@ -73,17 +73,17 @@
     </div>
     <img class="fixed flex items-center justify-center opacity-20 ml-16 mt-20 xl:-mt-10 xl:ml-[350px] xl:w-[50%] w-[60%] z-10 sm:ml-28 md:ml-36 lg:ml-48" src="/public/images/Logo_portefolio.png" alt="Logo">
 
-  <h1 class="font-bold text-lg mt-10 mb-5 lg:text-2xl">Réalisation de l'identité d'un restaurant (projet éducatif)</h1>
+  <h1 class="font-bold text-lg mt-10 mb-5 lg:text-2xl">Réalisation de l'identité graphique d'un restaurant (projet éducatif)</h1>
   <p class="opacity-80 text-sm lg:text-base lg:mr-[400px]">En collaboration avec trois camarades créatifs, j'ai eu le privilège de concevoir l'identité graphique d'un restaurant captivant. Notre équipe de quatre personnes a fusionné ses compétences en design pour donner vie à une vision commune, mêlant art culinaire et esthétique visuelle. Ensemble, nous avons exploré chaque élément, des logos aux menus en passant par le site internet, afin de capturer l'essence unique du restaurant et de créer une expérience visuelle cohérente et mémorable pour les clients. Cette collaboration dynamique a permis de combiner nos idées créatives individuelles pour aboutir à une identité graphique qui reflète parfaitement l'atmosphère et le style distinctifs de l'établissement.</p>
 
-  <div class="md:flex md:justify-center md:items-center md:gap-20 xl:gap-56 lg:ml-20 ">
+  <div class="md:flex md:justify-center md:items-center md:gap-20 xl:gap-56 lg:ml-20 lg:my-20">
     <div class="my-10 z-20">
       <img class="max-w-[120px] lg:max-w-md" src="/public/images/Logo_mg.png" alt="Image du Logo de Meet My Grill">
       <p class="text-center my-4 italic">Logo de Meet My Grill</p>
     </div>
 
     <div class="z-20">
-        <h2 class="font-bold text-lg lg:text-xl">Technique de tournage</h2>
+        <h2 class="font-bold text-lg lg:text-xl">Explication de l'identité graphique</h2>
       <p class="my-4">"Meet My Grill" incarne une véritable passion pour la viande rouge et le feu. L'identité graphique est une célébration de la flamboyance et de la qualité de la viande de bœuf. Le logo évoque l'énergie ardente et l'expertise culinaire qui animent notre établissement. Les nuances riches de orange rappellent la tendreté et la saveur incomparables de la nouriture. Chaque élément visuel, des menus aux enseignes, évoque l'atmosphère chaleureuse et l'excellence gastronomique de Meet My Grill. L'identité est un hommage vibrant à l'art de griller la viande et à la passion pour les saveurs authentiques du bœuf.</p>
     </div>
   </div>
@@ -91,13 +91,13 @@
   <div class="md:flex md:justify-center md:items-center md:gap-20 xl:gap-56 z-20">
     <div class="my-10 z-20">
       <div class="lg:mx-20 z-20">
-        <img class="max-w-[120px] lg:max-w-md" src="/public/images/Affiche_mg.png" alt="Image de l'affiche de Meet My Grill">
+        <img class="max-w-[120px] lg:max-w-xs" src="/public/images/Affiche_mg.png" alt="Image de l'affiche de Meet My Grill">
       </div>
       <p class="text-center text-sm italic my-4">Affiche</p>
     </div>
     <div class="my-10 z-20">
       <div class="lg:mx-20 z-20">
-        <img class="max-w-[120px] lg:max-w-md" src="/public/images/menu1_mg.png" alt="Image de présentation du menu">
+        <img class="max-w-[120px] lg:max-w-xs" src="/public/images/menu1_mg.png" alt="Image de présentation du menu">
       </div>
       <p class="text-center text-sm italic my-4">Menu de présentation</p>
     </div>
