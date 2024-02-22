0e826721003b4d251cb87d7d282d37692212161a
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-22 22:37:22 +0100

    Update MeetMyGrill.vue

--- src/views/MeetMyGrill.vue
+++ src/views/MeetMyGrill.vue
@@ -120,7 +120,7 @@
   </div>
   <div class="lg:mb-40 lg:mt-20 mx-10 xl:mx-20 z-20">
     <h2 class="font-bold text-lg lg:text-4xl my-10 mx-10 ">Galerie Photo</h2>
-    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-items-center items-center gap-10 lg:gap-36 xl:gap-64">
+    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-items-center items-center gap-10 lg:gap-36">
       <div class="lg:mx-10 z-20">
         <img class="max-w-[250px]  lg:max-w-md src="/public/images/menu2_mg.png" alt="Image du menu de Meet My Grill">
       </div>
