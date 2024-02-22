ee751ba89d8f54d7e902445908dfc35645115307
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-22 22:42:44 +0100

    Update MeetMyGrill.vue

--- src/views/MeetMyGrill.vue
+++ src/views/MeetMyGrill.vue
@@ -115,12 +115,12 @@
     </div>
   </div>
     
-  <div class="lg:w-1/2 lg:my-20 lg:ml-32 z-20">
+  <div class="lg:w-1/2 lg:my-20 lg:ml-24 z-20">
     <p>Nous avons créé une variété de supports de communication pour "Meet My Grill", notamment un dossier de communication soigneusement conçu. Ce dossier capture l'essence de notre identité centrée sur la viande rouge et le feu, avec des images vibrantes et une présentation claire de notre offre culinaire, notre menu et nos services. C'est un outil essentiel qui invite nos clients à découvrir et à vivre l'expérience "Meet My Grill" de manière immersive et mémorable.</p>
   </div>
   <div class="lg:mb-40 lg:mt-20 mx-10 xl:mx-20 z-20">
     <h2 class="font-bold text-lg lg:text-4xl my-10 mx-10 ">Galerie Photo</h2>
-    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-items-center items-center gap-10 lg:gap-36">
+    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-items-center items-center gap-10 lg:gap-20">
       <div class="lg:mx-10 z-20">
         <img class="max-w-[250px] lg:max-w-sm" src="/public/images/menu2_mg.png" alt="Image du menu de Meet My Grill">
       </div>
