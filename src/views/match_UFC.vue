3a62c632ffe7bdb9bc7c784918869dfc3dbb70d8
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-24 15:23:50 +0100

    Update match_UFC.vue

--- src/views/match_UFC.vue
+++ src/views/match_UFC.vue
@@ -95,7 +95,7 @@
     <h2 class="font-bold text-lg lg:text-4xl mt-10 mb-20 mx-10 ">Galerie Photo</h2>
     <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-items-center items-center gap-10 lg:gap-36 xl:gap-40">
       <div class="lg:mx-20 z-20">
-        <img class="max-w-[250px]  lg:max-w-md rounded-md" src="/public/images/ecusson.png" alt="Image de l'ecusson de l'équipe">
+        <img class="max-w-[150px]  lg:max-w-md rounded-md" src="/public/images/ecusson.png" alt="Image de l'ecusson de l'équipe">
       </div>
       <div class="lg:mx-20 z-20">
         <img class="max-w-[250px] lg:max-w-xs rounded-md" src="/public/images/t-shirt.jpg" alt="Image du t-shirt pour les fans">
