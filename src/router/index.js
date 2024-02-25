4ad4a98ef40389da39f7bcbc529fdd3bcc01fc48
Florentin Demortiere <95072018+Flo714@users.noreply.github.com>
2024-02-25 18:25:13 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/Accueil.vue'
 import Logo_Florentin_Demortiere from '../views/Logo_Florentin_Demortiere.vue'
-import Charte_graphique_UFC from '../views/Identité_graphique_UFC.vue'
+import Identité_graphique_UFC from '../views/Identité_graphique_UFC.vue'
 import MeetMyGrill from '../views/MeetMyGrill.vue'
 import drone_video from '../views/drone_video.vue'
 import Schoolastique from '../views/Schoolastique.vue'
@@ -16,7 +16,7 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'AccueilView', component: AccueilView },
     { path: '/Logo_Florentin_Demortiere', name: 'Logo_Florentin_Demortiere', component: Logo_Florentin_Demortiere },
-    { path: '/Identité_graphique_UFC', name: 'Charte_graphique_UFC', component: Charte_graphique_UFC },
+    { path: '/Identité_graphique_UFC', name: 'Identité_graphique_UFC', component: Identité_graphique_UFC },
     { path: '/MeetMyGrill', name: 'MeetMyGrill', component: MeetMyGrill },
     { path: '/drone_video', name: 'drone_video', component: drone_video },
     { path: '/Schoolastique', name: 'Schoolastique', component: Schoolastique },
